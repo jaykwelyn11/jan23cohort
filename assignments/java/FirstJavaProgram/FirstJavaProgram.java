f45c0e87da67a10c7338c1bb0cba8319d2e6af0c
jaykwelyn11 <jackie.sepulveda94@gmail.com>
2023-03-20 14:39:52 -0500

    add

---
+++ assignments/java/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+package CDAssignments.assignments.java;
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Jackie");
+        System.out.println("I am 28 years old");
+        System.out.println("My hometown is Fort Worth, TX");
+    }
+}
